e7814348464aa11abc5f0367f85dadcd80493b17
Ariel Rodriguez <r.ariel94@gmail.com>
2023-11-02 17:49:49 -0700

    completed required

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,8 +1,10 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 let firstName = 'Ariel';
+console.log(firstName);
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 let lastName = 'Rodriguez';
+console.log(lastName);
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
@@ -34,21 +36,26 @@ let pets = 1;
 console.log(pets);
 // 9 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
 let friendsPets =  2;
+console.log(friendsPets);
 // 10 - Add two pets to your `pets` variable
-pets = 3;
+pets = 5;
 console.log(pets)
 // 11 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 const allowedPets = 5;
+console.log(allowedPets);
 // 12 - Create a variable called `result`. Create a conditional:
 //      if adventurous is true, set `result` to be "Adventures are great!", 
 //      if it's not true,  set `result` to be "How about we stay home?"
 //      Console log the value of `result`
-let result = adventurous;
-if (result = true) {
-  console.log('Adventures are great!');
+
+
+let result = '';
+if (adventurous == true) {
+  result = 'Adventures are great!';
 } else {
-  console.log('How about we stay home');
+  result = 'How about we stay home';
 }
+console.log(result);
 
 // 13 - Create a variable called `diceRoll` and set it to the value of "Try again later.".
 //      Create a compound conditional: 
@@ -68,6 +75,17 @@ console.log(diceRoll);
 //      set `petStatus` to the value of "I have enough pets" 
 //      if the value of `pets` is greater than the value of `allowedPets`.
 //      set `petStatus` to the value of "Oh no, I have too many pets!"
+let petStatus = '';
+
+if (pets < allowedPets) {
+  petStatus = 'I can have more pets';
+} else if (pets == allowedPets) {
+  petStatus = 'I have enough pets';
+} else if (pets > allowedPets) {
+  petStatus = 'Oh no, I have too many pets!';
+}
+
+console.log (petStatus);
 
 
 // STRETCH GOALS:
@@ -77,6 +95,7 @@ console.log(diceRoll);
 //      assigns the highest value to `mostPets`. There's several possibilities --
 //      be sure to think through all the scenarios. 
 //      console.log `mostPets` after the conditional has run.
+
 
 // 16 - Make a variable called `luckyResult`
 //      Write a *switch* statement that sets `luckyResult` to:
